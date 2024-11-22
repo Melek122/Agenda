f31c972d84257e52d6bcff1f8c3b186d69dfadfa
khalil830 <60982897+khalil830@users.noreply.github.com>
2024-11-22 01:27:56 +0100

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 // db.php for Azure MySQL Database
 
 $host = 'agenda-server.mysql.database.azure.com';  // Fully qualified domain name of your MySQL server
-$dbname = 'agenda-database';  // Replace with your actual database name
+$dbname = 'agenda-server';  // Replace with your actual database name
 $user = 'cnvjhyjscs@agenda-server.mysql.database.azure.com';  // Include @servername for Azure MySQL username
 $pass = 'fuckilyes123+';  // Your Azure MySQL password
 $ssl_cert = 'DigiCertGlobalRootCA.crt.pem';  // Path to the downloaded SSL certificate
