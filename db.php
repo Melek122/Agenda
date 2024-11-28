539eabd8d0bd983495c3a2841dc67ff7ee3399f0
khalil830 <60982897+khalil830@users.noreply.github.com>
2024-11-28 15:21:26 +0100

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 // db.php for Azure MySQL Database
 
 $host = 'agenda-server.mysql.database.azure.com';  // Fully qualified domain name of your MySQL server
-$dbname = 'agenda-database';  // Replace with your actual database name
+$dbname = 'agenda_app';  // Replace with your actual database name
 $user = 'fbwgcxxxjl';  // Include @servername for Azure MySQL username
 $pass = 'Test123+';  // Your Azure MySQL password
 
