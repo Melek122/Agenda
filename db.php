01f83249bf88b7fa5aa62527aaa728716521782a
khalil830 <60982897+khalil830@users.noreply.github.com>
2024-11-28 16:16:11 +0100

    Update db.php

--- db.php
+++ db.php
@@ -7,7 +7,7 @@ $user = 'fbwgcxxxjl@agenda-server';  // Include @servername for Azure MySQL user
 $pass = 'Test123+';  // Your Azure MySQL password
 $dsn = "mysql:host=$host;dbname=$dbname;port=3306"; 
 $options = [
-    PDO::MYSQL_ATTR_SSL_CA => 'DigiCertGlobalRootG2.crt.pem', // Path to SSL certificate
+    PDO::MYSQL_ATTR_SSL_CA => 'BaltimoreCyberTrustRoot.crt.pem', // Path to SSL certificate
 ];
 
 try {
