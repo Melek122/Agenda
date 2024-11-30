0519a33ff2b24b245753c904f762b08c251b3a44
khalil830 <60982897+khalil830@users.noreply.github.com>
2024-11-30 22:08:27 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,9 @@
 <?php
 // db.php for Azure MySQL Database (without SSL)
 
-$host = 'agenda-app-server.mysql.database.azure.com';  // Fully qualified domain name of your MySQL server
-$dbname = 'agenda-app-database';  
-$user = 'ddhquucrom';  // Include @servername for Azure MySQL username
-$pass = 'Test123+';  // Your Azure MySQL password
-
-// DSN (Data Source Name)
-$dsn = "mysql:host=$host;dbname=$dbname;port=3306;charset=utf8mb4";
+$host = getenv('agenda-app-server.mysql.database.azure.com');       // Set in Azure app settings
+$dbname = getenv('agenda-app-database');     // Set in Azure app settings
+$user = getenv('ddhquucrom');       // Set in Azure app settings
+$pass = getenv('Test123+');  
 
 ?>
