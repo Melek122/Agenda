7d2640ad4ef0c5ea9c1307d4c40b422e412fd267
khalil830 <60982897+khalil830@users.noreply.github.com>
2024-12-01 18:53:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -211,7 +211,7 @@ $stmt->close();
                             </td>
                             <td>
                                 <a href="edit_event.php?id=<?php echo $event['id']; ?>" class="btn btn-edit btn-sm">Edit</a>
-                                <a href="delete_event.php?id=<?php echo $event['id']; ?>" class="btn btn-danger btn-sm">Delete</a>
+                                <a href="delete_event.php?event_id=<?php echo $event['id']; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this event?');">Delete</a>
                             </td>
                         </tr>
                     <?php endforeach; ?>
